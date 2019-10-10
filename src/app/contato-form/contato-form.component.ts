e5dca744467f97a066088a397ab97ef2cfa7ef98
Haroldo Beyer <haroldobeyerbacellar@gmail.com>
2019-10-10 19:56:41 -0300

    Inserindo serviço

--- src/app/contato-form/contato-form.component.ts
+++ src/app/contato-form/contato-form.component.ts
@@ -1,4 +1,5 @@
-import { Contato } from "./../../models/Contato";
+import { ContatoService } from "./../services/contato.service";
+import { Contato } from "../../models/contato.model";
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
@@ -9,13 +10,18 @@ import { NgForm } from "@angular/forms";
 })
 export class ContatoFormComponent implements OnInit {
   contato: Contato;
-  constructor() {}
+  contatos: Contato[];
+
+  constructor(private contatoService: ContatoService) {}
 
   ngOnInit() {
     this.contato = new Contato();
   }
 
   onSubmit(form: NgForm) {
+    this.contato = form.value;
+    this.contato.id = new Date().getTime().toString();
+    this.contatoService.save(this.contato);
     console.log(form.value);
   }
 }
